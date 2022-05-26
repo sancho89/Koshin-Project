54037032d8360b8616d57359cf723270991622ff
KevinBB22 <blancodaw22@gmail.com>
2022-05-26 19:47:56 +0200

    Añadiendo el javaScript a contactos

--- js/main.js
+++ js/main.js
@@ -25,6 +25,53 @@
         }
     });
 
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    //Variables
+    const btnEnviar = document.querySelector('#enviar');
+
+    // Variables para campos
+
+    const nombre = document.querySelector('nombreC');
+    const email = document.querySelector('emailC');
+    const asunto = document.querySelector('asuntoC');
+    const mensaje = document.querySelector('mensajeC');
+
+    // Campos del formulario
+    email.addEventListener('blur' , validarFormulario);
+
+    // Validacion del formulario
+    function validarFormulario(e) {
+        
+    }
+    
+    
+    // Funcione 
+    eventListeners();
+    function eventListeners() {
+        document.addEventListener('DOMContentLoaded', BotonDisabled);
+    }
+
+    // Funciones
+    function BotonDisabled() {
+        btnEnviar.disabled = true;
+        btnEnviar.classList.add('cursor-not-allowed')
+    }
+
+
+
+
     // Testimonials carousel
     $('.testimonial-carousel').owlCarousel({
         autoplay: true,
