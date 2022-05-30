19defd0561830f643d49a0aebe57acbc2ce437a3
KevinBB22 <blancodaw22@gmail.com>
2022-05-30 20:41:22 +0200

    Formulario añadiendo la validacion de datos javascript

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 (function ($) {
     "use strict";
 
@@ -40,35 +41,80 @@
 
     //Variables
     const btnEnviar = document.querySelector('#enviar');
+    const formulario = document.querySelector('#formC')
 
     // Variables para campos
 
-    const nombre = document.querySelector('nombreC');
-    const email = document.querySelector('emailC');
-    const asunto = document.querySelector('asuntoC');
-    const mensaje = document.querySelector('mensajeC');
+    const nombre = document.querySelector('#nombreC');
+    const email = document.querySelector('#emailC');
+    const asunto = document.querySelector('#asuntoC');
+    const mensaje = document.querySelector('#mensajeC');
 
-    // Campos del formulario
-    email.addEventListener('blur' , validarFormulario);
 
-    // Validacion del formulario
-    function validarFormulario(e) {
-        
-    }
-    
-    
     // Funcione 
     eventListeners();
     function eventListeners() {
         document.addEventListener('DOMContentLoaded', BotonDisabled);
     }
 
+    // Campos del formulario
+    email.addEventListener('blur', validarFormulario);
+    nombre.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
+    
+
     // Funciones
     function BotonDisabled() {
         btnEnviar.disabled = true;
         btnEnviar.classList.add('cursor-not-allowed')
     }
+     
+    // Validacion del formulario
+    function validarFormulario(e) {
+        if(e.target.value.length > 0){
 
+            const error = document.querySelector('p.error');
+            error.remove();
+                
+            
+            console.log('Si hay texto')
+            e.target.style.borderBottomColor = '#6C7293';
+    
+        }else{
+                e.target.style.borderBottomColor = 'red';
+                mostrarError('Todos los campos han de ser rellenados');
+    
+        }
+
+        if(e.target.type === 'email'){
+            const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            
+            if(er.test( e.target.value)) {
+
+                const error = document.querySelector('p.error');
+                error.remove();
+                console.log('email valido');
+            }else {
+                e.target.style.borderBottomColor = 'red';
+                mostrarError('El email no es valido');
+                
+            }
+        }
+    }
+
+    function mostrarError(mensaje){
+        const mensajeMal = document.createElement('p');
+        mensajeMal.textContent = mensaje;
+        mensajeMal.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5' , 'text-center' , 'error');
+
+        const errores = document.querySelectorAll('.error');
+        if(errores.length === 0) {
+            formulario.appendChild(mensajeMal);
+        }
+        
+        
+    }
 
 
 
@@ -85,3 +131,5 @@
 
     
 })(jQuery);
+
+  
