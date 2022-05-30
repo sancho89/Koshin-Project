d70cea72c9101e1e480c23466ed7338d4044c9e0
carloslazaroam <carloslazaroam@gmail.com>
2022-05-30 21:12:57 +0200

    empezando equipos modo admin

---
+++ js/equipos.js
@@ -0,0 +1,26 @@
+var  divtitulo = document.getElementById("titulo");
+
+
+var divparrafo = document.getElementById("parrafo");
+
+
+    document.getElementByID("botontitulo").onclick=cambiarTitulo;
+
+
+
+    document.getElementByID("botonparrafo").onclick=cambiarParrafo;
+
+var nuevotitulo;
+function cambiarTitulo(){
+var nuevotitulo;
+divtitulo=prompt('Escribe el nuevo titulo que deseas cambiar','');
+alert('El nuevo titulo es: ' + divtitulo)
+}
+
+function cambiarParrafo(){
+var nuevoparrafo;
+nuevoparrafo=prompt('Escribe el nuevo parrafo que deseas cambiar');
+nuevoparrafo.inner
+
+}
+
