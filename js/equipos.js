c61065315ce8bae88d755c7453b6d922a6aad918
carloslazaroam <carloslazaroam@gmail.com>
2022-06-01 17:47:00 +0200

    Botones admin titulo y descripcion del equipo

--- js/equipos.js
+++ js/equipos.js
@@ -10,17 +10,36 @@ var divparrafo = document.getElementById("parrafo");
 
     document.getElementByID("botonparrafo").onclick=cambiarParrafo;
 
-var nuevotitulo;
+
 function cambiarTitulo(){
 var nuevotitulo;
 divtitulo=prompt('Escribe el nuevo titulo que deseas cambiar','');
-alert('El nuevo titulo es: ' + divtitulo)
+
+
+if (divtitulo == '') {
+    alert('No puedes dejar el titulo vacio, debes introdcir algo')
+    document.getElementById('titulo');
+} else {
+    alert('El nuevo titulo es: ' + divtitulo)
+document.getElementById('titulo').innerHTML= divtitulo;
+}
+
 }
 
 function cambiarParrafo(){
-var nuevoparrafo;
-nuevoparrafo=prompt('Escribe el nuevo parrafo que deseas cambiar');
-nuevoparrafo.inner
+divparrafo=prompt('Escribe el nuevo parrafo que deseas cambiar','');
+if (divparrafo == '' ){
+    alert('No puedes dejar la descripción vacia, debes introdcir algo')
+    document.getElementById('parrafo');
+}
+
+else {
+alert('El nuevo parrafo es: ' + divparrafo)
+document.getElementById('parrafo').innerHTML= divparrafo;
+
+}
+
+
 
 }
 
